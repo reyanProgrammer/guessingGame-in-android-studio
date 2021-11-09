7732b35ed4508c4702816a77694330205b9de50b
reyanProgrammer <reyanmehmood8@gmail.com>
2021-11-09 12:28:16 +0500

    bug fixed

--- app/src/main/java/com/reyan/guessinggame/MainActivity.java
+++ app/src/main/java/com/reyan/guessinggame/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
                     result.setText("you win");
                 }else {
                     result.setText("you lose");
+                    button.setEnabled(false);
                 }
             }
         });
